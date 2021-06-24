6fe72915ca71dd4c3a2dee6cef3bd837b7c442c9
cnuhoya <dk00426@naver.com>
2021-06-24 22:24:39 +0900

    Update PolicyHandler.java

--- space/src/main/java/spacerent/PolicyHandler.java
+++ space/src/main/java/spacerent/PolicyHandler.java
@@ -17,33 +17,34 @@ public class PolicyHandler{
 
       
         if(!approved.validate()) return;
-
-        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getBookid() + "\n\n");
-        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getSpacename() + "\n\n");
-        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getStatus() + "\n\n");
-        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getBookid() + "\n\n");
-        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getBookid() + "\n\n");             
-        
-        if(approved.getStatus().equals("cancel-booking")){
-          System.out.println("\n\n55555555555555555555555 : " + approved.getStatus() + "\n\n");
-          Space space = spaceRepository.findByBookid(approved.getBookid());
-          space.setUserid(approved.getUserid());         
-          space.setSpacename(approved.getSpacename());         
-          space.setBookid(approved.getBookid());                 
-          space.setStatus("cancel-register");   
-          spaceRepository.save(space);        
-        }else{
-          System.out.println("\n\n666666666666666666666 : " + approved.getStatus() + "\n\n");
+       
+      
           Space space = new Space();
           space.setStatus("success-register");
           space.setUserid(approved.getUserid());         
           space.setSpacename(approved.getSpacename());         
           space.setBookid(approved.getBookid());
           spaceRepository.save(space);                    
-        } 
+    
                   
     
     }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverCancelled_cancelspace(@Payload Cancelled cancelled){
+
+      
+        if(!cancelled.validate()) return;
+          
+        
+        if(cancelled.getStatus().equals("cancel-pay")){
+          Space space = spaceRepository.findByBookid(cancelled.getBookid());
+          space.setStatus("cancel-register");   
+          spaceRepository.save(space);        
+        }
+                  
+    
+    }    
 
 
     @StreamListener(KafkaProcessor.INPUT)
