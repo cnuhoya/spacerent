b8c14d9a6073ed5710d267c74b9346ded7b37875
cnuhoya <dk00426@naver.com>
2021-06-24 21:31:11 +0900

    Update PolicyHandler.java

--- space/src/main/java/spacerent/PolicyHandler.java
+++ space/src/main/java/spacerent/PolicyHandler.java
@@ -15,23 +15,34 @@ public class PolicyHandler{
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverApproved_Receivestatus(@Payload Approved approved){
 
+      
         if(!approved.validate()) return;
 
-        System.out.println("\n\n##### listener Receivestatus : " + approved.toJson() + "\n\n");        
-        Space space = spaceRepository.findByBookid(approved.getBookid()); 
-
-//        space.setUserid(approved.getUserid());
-//        space.setSpacename(approved.getSpacename());
-//        space.setBookid(approved.getBookid());
-
-        if(approved.getStatus() == "cancel-pay"){
-           space.setStatus("cancel-register");          
-        }
-        else{
+        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getBookid() + "\n\n");
+        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getSpacename() + "\n\n");
+        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getStatus() + "\n\n");
+        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getBookid() + "\n\n");
+        System.out.println("\n\n$$$$$$ APRROVED BOOK ID :  " + approved.getBookid() + "\n\n");             
+        
+        if(approved.getStatus().equals("cancel-booking")){
+          System.out.println("\n\n55555555555555555555555 : " + approved.getStatus() + "\n\n");
+          Space space = spaceRepository.findByBookid(approved.getBookid());
+          space.setUserid(approved.getUserid());         
+          space.setSpacename(approved.getSpacename());         
+          space.setBookid(approved.getBookid());                 
+          space.setStatus("cancel-register");   
+          spaceRepository.save(space);        
+        }else{
+          System.out.println("\n\n666666666666666666666 : " + approved.getStatus() + "\n\n");
+          Space space = new Space();
           space.setStatus("success-register");
+          space.setUserid(approved.getUserid());         
+          space.setSpacename(approved.getSpacename());         
+          space.setBookid(approved.getBookid());
+          spaceRepository.save(space);                    
         } 
                   
-        spaceRepository.save(space);    
+    
     }
 
 
